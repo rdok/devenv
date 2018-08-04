f4492bd9eb1a2c262e071e8355a0cbd24ee2bec9
Rizart Dokollari <r.dokollari@gmail.com>
2018-08-04 20:56:49 +0100

    Feature: ability to execute all tests. See #2.

--- run_tests.bash
+++ run_tests.bash
@@ -1,7 +1,19 @@
+#!/usr/bin/env bash
+
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 testsPath="$dir/tests"
+resultsLogPath="$testsPath/_output/results.log"
+blue="\e[1;34m"
+resetColor="\e[0m"
+
+echo '' > "$resultsLogPath"
 
 for testPath in $testsPath/*_test; do
-    printf "Executing %s\n" $testPath
-    bash "$testFile" 2>&1 | tee "$testsPath/_output/results.log"
+    output="\n"
+    output+=$(printf "${blue}%0.s-${resetColor}" {1..80})
+    output+="\nTest $testPath\n"
+    output+=$(printf "${blue}%0.s-${resetColor}" {1..80})
+    echo -e $output
+    echo -e $output >> $resultsLogPath
+    bash "$testPath" 2>&1 | tee -a "$resultsLogPath"
 done
