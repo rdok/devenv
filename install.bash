d819dbad57349f561c66dc2529a82181f72b5503
Rizart Dokollari <r.dokollari@gmail.com>
2018-08-05 13:44:29 +0100

    Boyscouting installer script, more readable infos.

--- install.bash
+++ install.bash
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
 scriptDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+. "$scriptDir/src/logger.inc.bash"
 
+
+notice "Initializing..."
 . "$scriptDir/src/bash_prompt.inc.bash"
 . "$scriptDir/src/bashrc.inc.bash"
 . "$scriptDir/src/file.inc.bash"
-. "$scriptDir/src/logger.inc.bash"
+
 
 info 'Installing the bash_prompt...'
 appendStringToFile "$(generatePs1)" "$(bashPromptPath)"
 
-info 'Instructing your system to use bash_prompt...'
+
+info 'Instructing the os to use bash_prompt...'
 appendStringToFile "$(bashPromptSrcCmd)" "$(bashrcPath)"
 
-notice 'Done'
+
+notice "Done\n"
 
