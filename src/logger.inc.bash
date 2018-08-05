3f11e750bd6e5d10a6fdd344f8913de37b384b05
Rizart Dokollari <r.dokollari@gmail.com>
2018-08-05 12:52:05 +0100

    Boyscouting logger: describe variables for colors.

--- src/logger.inc.bash
+++ src/logger.inc.bash
@@ -1,11 +1,15 @@
+resetColor="\e[0m"
+
 function info() {
     message="$1"
+    defaultColor="\e[1;39m"
 
-    echo -e "\e[1;39m${message}\e[0m"
+    echo -e "${defaultColor}${message}${resetColor}"
 }
 
 function notice() {
     message="$1"
+    greenColor="\e[0;32m"
 
-    echo -e "\e[0;32m${message}\e[0m"
+    echo -e "\e[0;32m${message}${resetColor}"
 }
