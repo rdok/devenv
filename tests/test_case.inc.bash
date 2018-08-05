f70132b95075f81e1e44d5756f4b1b41dfaa950e
Rizart Dokollari <r.dokollari@gmail.com>
2018-08-05 11:38:49 +0100

    Resolve #6. TestHelper: assert a file path corresponds to a valid file.

--- tests/test_case.inc.bash
+++ tests/test_case.inc.bash
@@ -3,6 +3,7 @@ assertFileContainsString() {
     string="$2"
 
     assertStringNotEmpty $string
+    assertFileIsValid $file
 
     fileContainsString=$(cat "$file" | \ 
         grep -xqFe "$string" && \
@@ -23,4 +24,16 @@ assertStringNotEmpty() {
     message=$(printf "Failed asserting the given string is not empty.")
 
     assertFalse "$message" "$stringIsEmpty"
+}
+
+assertFileIsValid() {
+    file="$1"
+
+    assertStringNotEmpty "$file"
+
+    isFile=$([[ -f "$file" ]] && echo true || echo false)
+
+    message=$(printf "Failed asserting file '%s' is valid." "$file")
+
+    assertTrue "$message" "$isFile"
 }
